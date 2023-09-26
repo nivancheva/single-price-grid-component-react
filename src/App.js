2bf3d2e80ae9399bfd737efd616c49d9fb375a43
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-09-26 15:51:18 +0300

    add jsx to App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <main>
+      <div className='card-one'>
+        <h1>Join our community</h1>
+        <p>30-day, hassle-free money back guarantee</p>
+        <p>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+      </div>
+
+      <div className='card-two'>
+        <h3>Monthly Subscription</h3>
+        <p> <span>&dollar;29</span> per month</p>
+        <p>Full access for less than &dollar;1 a day</p>
+        <button>Sign Up</button>
+      </div>
+
+      <div className='card-three'>
+        <h3>Why Us</h3>
+        <p>Tutorials by industry experts Peer &amp; expert code review Coding exercises Access to our GitHub repos Community forum Flashcard decks New videos every week</p>
+      </div>
+    </main>
   );
 }
 
